7e7f4f4914b7dede3b729ee8dd7e3de2aff5281d
RochaGah <gabyrochanunes@gmail.com>
2025-08-19 10:26:43 -0300

    reversão-das-mudanças-acidentais

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from "react-router";
 
-import AuthProvider from "./providers/AuthProvider";
+import AuthProvider from './providers/AuthProvider';
 
-import Home from "./pages/home";
-import Login from "./pages/login";
-import UsersLayout from "./pages/admin/users/layout";
-import User from "./pages/admin/users";
-import NewUser from "./pages/admin/users/new";
-import AdminLayout from "./pages/admin/layout";
-import Admin from "./pages/admin";
-import Rooms from "./pages/admin/rooms";
-import NewRoom from "./pages/admin/rooms/new";
-import Layout from "./pages/layout";
+import Home from './pages/home';
+import Login from './pages/login';
+import UsersLayout from './pages/admin/users/layout';
+import User from './pages/admin/users';
+import NewUser from './pages/admin/users/new';
+import AdminLayout from './pages/admin/layout';
+import Admin from './pages/admin';
+import Rooms from './pages/admin/rooms';
+import NewRoom from './pages/admin/rooms/new';
+import Layout from './pages/layout';
 
-import "./index.css";
-import RoomsProvider from "./providers/RoomsProvider";
-import Room from "./pages/room";
-import ChangePassword from "./pages/admin/users/change-password";
+import './index.css'
+import RoomsProvider from './providers/RoomsProvider';
+import Room from './pages/room';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
@@ -36,7 +35,6 @@ createRoot(document.getElementById("root")!).render(
                 <Route path="users" element={<UsersLayout />}>
                   <Route index element={<User />} />
                   <Route path="new" element={<NewUser />} />
-                  <Route path="change-password" element={<ChangePassword />} />
                 </Route>
                 <Route path="rooms" element={<Rooms />}>
                   <Route index element={<Rooms />} />
@@ -49,4 +47,4 @@ createRoot(document.getElementById("root")!).render(
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
-);
+)
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,53 +1,29 @@
-import * as z from "zod";
+import * as z from 'zod'
 
 // Schema padrão para cadastro de usuários
-export const registerSchema = z
-  .object({
-    email: z
-      .email({ message: "Informe um e-mail válido" })
-      .nonempty({ message: "Informe um e-mail válido" }),
-    password: z
-      .string()
-      .min(8, { message: "As senhas contém pelo menos 8 dígitos" }),
-    confirm: z.string().min(8, { message: "Você precisa confirmar sua senha" }),
-  })
-  .refine((data) => data.password === data.confirm, {
-    message: "As senhas não coincidem. Verifique os dados e tente novamente",
-  });
+export const registerSchema = z.object({
+  email: z.email({message: "Informe um e-mail válido"}).nonempty({message: "Informe um e-mail válido"}),
+  password: z.string().min(8, {message: "As senhas contém pelo menos 8 dígitos"}),
+  confirm: z.string().min(8, {message: "Você precisa confirmar sua senha"})
+})
+.refine((data) => data.password === data.confirm, {
+  message: "As senhas não coincidem. Verifique os dados e tente novamente"
+})
 
 //Usando a função omit, eu posso reaproveitar o schema de cadastro para o login, removendo (omitindo) os campos que não são necessários nesse cenário.
-export const loginSchema = registerSchema.omit({ confirm: true });
-
-export const changePasswordSchema = z
-  .object({
-    password: z
-      .string()
-      .min(8, { message: "As senhas contém pelo menos 8 dígitos" }),
-    newPassword: z
-      .string()
-      .min(8, { message: "As senhas contém pelo menos 8 dígitos" }),
-    confirmPassword: z
-      .string()
-      .min(8, { message: "As senhas contém pelo menos 8 dígitos" }),
-  })
-  .refine((data) => data.newPassword === data.confirmPassword, {
-    message: "As senhas não coincidem. Verifique os dados e tente novamente",
-  });
+export const loginSchema = registerSchema.omit({confirm: true})
 
 export const allowedUserSchema = z.object({
-  user: z.string().regex(/[A-Z][a-z].* [A-Z][a-z].*/, {
-    message: "Forneça nome e sobrenome",
-  }),
-  valid_until: z.string().optional(),
-});
+  user: z.string().regex(/[A-Z][a-z].* [A-Z][a-z].*/, {message: "Forneça nome e sobrenome"}),
+  valid_until: z.string().optional()
+})
 
-export type loginSchemaType = z.infer<typeof loginSchema>;
-export type changePasswordSchemaType = z.infer<typeof changePasswordSchema>;
-export type registerSchemaType = z.infer<typeof registerSchema>;
-export type allowedUserType = z.infer<typeof allowedUserSchema>;
+export type loginSchemaType = z.infer<typeof loginSchema>
+export type registerSchemaType = z.infer<typeof registerSchema>
+export type allowedUserType = z.infer<typeof allowedUserSchema>
 
-export interface AllowedUser {
-  id: number;
-  user: string;
-  valid_until?: string | null;
+export  interface AllowedUser {
+  id: number
+  user: string
+  valid_until?: string | null
 }
