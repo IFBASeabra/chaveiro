0d1297b7954fda0e3f00e26b7198b5d586023fce
RochaGah <gabyrochanunes@gmail.com>
2025-08-19 10:09:35 -0300

    pagina-mudar-senha-não-terminada

---
+++ src/pages/admin/users/change-password/layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+const ChangePasswordLayout = () => {
+  return (
+    <>
+      changePassword
+      <Outlet />
+    </>
+  );
+};
+
+export default ChangePasswordLayout;
